5aa44f18668c1a28a5c485163d61481a125578ca
maheenTaher-mtk <maheentaher764@gmail.com>
2024-04-19 22:45:53 -0400

    added the true or false exercise

---
+++ src/trueOrFalse.java
@@ -0,0 +1,30 @@
+//Maheen Taher, 2024
+import javax.swing.JOptionPane;
+
+public class trueOrFalse {
+    public static void main(String[] args) throws Exception {
+    //defining the validity of the user's opinion as false from the start of the program so that it can loop until the validity is deemed true
+    boolean validOpinion = false;
+    //loop that breaks when the opinion is deemed true
+    while (validOpinion == false){
+        //prompt user t/f question
+        int opinion = JOptionPane.showConfirmDialog(null, "Is Ms. Richards an awesome teacher?", "Urgent True or False Question!", JOptionPane.YES_NO_OPTION);
+        
+        //if user answers yes, display a congratulatory message and break the loop
+        if (opinion == JOptionPane.YES_OPTION){
+        JOptionPane.showMessageDialog(null, "You are correct!", "Congratulations!", JOptionPane.INFORMATION_MESSAGE);
+        validOpinion = true;
+        } 
+
+        //if user attempts to close the prompt, display an error message
+        else if (opinion == JOptionPane.CLOSED_OPTION){
+        JOptionPane.showMessageDialog(null, "You gotta answer the question bro 😭", "HEY!", JOptionPane.ERROR_MESSAGE);    
+        }
+
+        //otherwise (if the user answers no), display an error message
+        else {
+        JOptionPane.showMessageDialog(null, "Ermm... please try again.", "WRONG!", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    }
+}
